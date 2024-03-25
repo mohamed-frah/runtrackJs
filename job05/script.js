1952bdc427847ae3c6f4b07cadc7c390381e7c4e
mohamed-frah <mohamed.frah@laplateformeio>
2024-03-25 11:55:53 +0100

    j'ai mis la boucle for

--- job05/script.js
+++ job05/script.js
@@ -1,7 +1,9 @@
 function les_Jours_de_semain(){
     var jourSemain = ["Lundi","Mardi", "Mercredi","Jeudi","Vendredi", "Samdi" ]
-    
+    for(var i =0 ; i < jourSemain.length ; i++) {
+        console.log(jourSemain[i])
 
-
-
+    }
 }
+
+les_Jours_de_semain(); 
